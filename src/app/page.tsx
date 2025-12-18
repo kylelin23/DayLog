14c641376c588c2de2b6934a1147d1e51732d327
Kyle Lin <linkyle0924@gmail.com>
2025-12-18 00:27:10 -0800

    feat: add sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
-import Link from "next/link";
+"use client";
+
+import style from "./signIn.module.css"
+import { useRouter } from "next/navigation";
 
 export default function LogIn() {
+  const router = useRouter();
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    router.push("/home/");
+  }
+
   return (
-    <div>
-      <Link href = "/home/">Log In</Link>
+    <div className = {style.container}>
+      <div className = {style.signInContainer}>
+        <div className = {style.signInBar}>
+          Sign In
+        </div>
+        {/* <div className = {style.signInComponents}> */}
+           <form id="contact-form" className = {style.signInComponents}>
+            <label className={style.text} htmlFor="email">Email</label>
+            <br /><br />
+            <input
+              className={style.input}
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <br /><br />
+
+            <label className={style.text} htmlFor="password">Password</label>
+            <br /><br />
+            <input
+              className={style.input}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+            <br /><br />
+
+            <input
+              className={style.submit}
+              type="submit"
+              onClick = {handleClick}
+              value="Submit"
+            />
+           </form>
+        {/* </div> */}
+      </div>
     </div>
+
   );
 }
