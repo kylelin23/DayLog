8f19fd85b1e716e5d7ad83bbc99d755fb7e3bfa8
Kyle Lin <linkyle0924@gmail.com>
2025-12-19 12:51:33 -0800

    feat: add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,25 @@
 "use client";
 
+import { SignInButton, SignUpButton, SignedOut } from '@clerk/nextjs'
 import style from "./signIn.module.css"
-import { useRouter } from "next/navigation";
 
 export default function LogIn() {
-  const router = useRouter();
-
-  const handleClick = (e: any) => {
-    e.preventDefault();
-    router.push("/home/");
-  }
 
   return (
-    <div className = {style.container}>
-      <div className = {style.signInContainer}>
-        <div className = {style.signInBar}>
-          Sign In
-        </div>
-        <form id="contact-form" className = {style.signInComponents}>
-          <label className={style.text} htmlFor="email">Email</label>
-          <input
-            className={style.input}
-            type="text"
-            id="email"
-            name="email"
-            placeholder="Email"
-            required
-          />
 
-          <label className={style.text} htmlFor="password">Password</label>
-          <input
-            className={style.input}
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-          />
-
-          <input
-            className={style.submit}
-            type="submit"
-            onClick = {handleClick}
-            value="Submit"
-          />
-        </form>
-      </div>
+    <div className= {style.container}>
+      <SignedOut>
+        <SignInButton>
+          <button className = {style.button} style = {{backgroundColor: 'white'}}>
+            Sign In
+          </button>
+        </SignInButton>
+        <SignUpButton >
+          <button className = {style.button} style = {{backgroundColor: 'lightSkyBlue'}}>
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
     </div>
 
   );
--- src/components/Top Bar/TopBar.tsx
+++ src/components/Top Bar/TopBar.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link'
 import style from './TopBar.module.css'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 export default function TopBar() {
     return(
         <header className={style.navbar}>
-            <h1><Link href="/" className={style.logo}>DayLog</Link></h1>
+            <h1>
+                <Link href="/home" className={style.logo}>DayLog</Link>
+            </h1>
+            <div className = {style.userButton}>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
         </header>
     )
 }
